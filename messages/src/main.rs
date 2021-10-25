62474081c5336c101f22c7f95fc811f47b00637a
Andrew Rzeznik <ajrzeznik@gmail.com>
2021-10-24 21:59:52 -0400

    Updates to get auto-message building operational

--- messages/src/main.rs
+++ messages/src/main.rs
@@ -1,3 +1,22 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world! I'm at {:?}", std::env::current_dir().unwrap());
+
+    let output = if cfg!(target_os = "windows") {
+        Command::new("flatc.exe")
+            .args(["-r", "--gen-object-api", "-o", "generated", ".\\src\\message_definitions\\node_address.fbs"])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("flatc")
+            .arg("-c")
+            .arg("echo hello")
+            .output()
+            .expect("failed to execute process")
+    };
+
+    let hello = output.stdout;
+    let result_string = std::str::from_utf8(&hello).unwrap();
+    println!("Current directory: {:?}", std::env::current_dir().unwrap());
+    println!("{}", result_string);
 }
