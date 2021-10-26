8bc4ca8cb12c663088c0cdfa62fded5629a9dc55
Andrew Rzeznik <ajrzeznik@gmail.com>
2021-10-26 09:30:11 -0400

    Updated tests for port reuse

--- min_mq/src/lib.rs
+++ min_mq/src/lib.rs
@@ -35,13 +35,17 @@ mod tests {
     #[test]
     //TODO: Clean this up a bit, remove the unwraps and have proper asserts
     fn send_and_receive() {
-        let receive_thread = thread::spawn(|| {
+        let receive_thread_1 = thread::spawn(|| {
+            assert!(receive_broadcast().is_ok())
+        });
+        let receive_thread_2 = thread::spawn(|| {
             assert!(receive_broadcast().is_ok())
         });
         //TODO: Have a synchronization method to test this instead of a wait
         thread::sleep(Duration::from_millis(1000));
 
         assert!(broadcast_address().is_ok());
-        assert!(receive_thread.join().is_ok());
+        assert!(receive_thread_1.join().is_ok());
+        assert!(receive_thread_2.join().is_ok());
     }
 }
