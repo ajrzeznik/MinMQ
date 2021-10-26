ced8a032d4cd3fffbb2c03db56822167bcd7c870
Andrew Rzeznik <ajrzeznik@gmail.com>
2021-10-26 08:43:52 -0400

    Using the dnyamic discover port now

--- min_mq/src/lib.rs
+++ min_mq/src/lib.rs
@@ -7,13 +7,13 @@ pub fn broadcast_address() -> io::Result<()> {
     let socket : UdpSocket = UdpSocket::bind("0.0.0.0:0")?;
     socket.set_broadcast(true)?;
     let call = vec![2,3,5,7];
-    socket.send_to(&call, "255.255.255.255:55555").unwrap();
+    socket.send_to(&call, format!("255.255.255.255:{}", DYNAMIC_DISCOVER_PORT)).unwrap();
     Ok(())
 }
 
 //TODO: Clean this up to reuse the bound port, since that's needed to listen for messages
 pub fn receive_broadcast() -> io::Result<()> {
-    let socket : UdpSocket = UdpSocket::bind("0.0.0.0:55555")?;
+    let socket : UdpSocket = UdpSocket::bind(format!("0.0.0.0:{}", DYNAMIC_DISCOVER_PORT))?;
     let mut buf = [0; 100];
     let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).unwrap();
     println!("Received: {:?}", &buf[0..number_of_bytes]);
