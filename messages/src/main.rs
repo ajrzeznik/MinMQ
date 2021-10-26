e39f28bdce747709fcf201b6967d4162decf44b3
Andrew Rzeznik <ajrzeznik@gmail.com>
2021-10-26 08:27:04 -0400

    Multi-crate building

--- messages/src/main.rs
+++ messages/src/main.rs
@@ -2,20 +2,4 @@ use std::process::Command;
 
 fn main() {
 
-    let output = if cfg!(target_os = "windows") {
-        Command::new("flatc.exe")
-            .args(["-r", "--gen-object-api", "-o", "generated", ".\\src\\message_definitions\\NodeAddress.fbs"])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("flatc")
-            .args(["-r", "--gen-object-api", "-o", "generated", "./src/message_definitions/NodeAddress.fbs"])
-            .output()
-            .expect("failed to execute process")
-    };
-
-    let hello = output.stdout;
-    let result_string = std::str::from_utf8(&hello).unwrap();
-    println!("Current directory: {:?}", std::env::current_dir().unwrap());
-    println!("{}", result_string);
 }
